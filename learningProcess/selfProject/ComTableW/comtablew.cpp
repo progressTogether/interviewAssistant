038e2691ec1c815c08cb4f88a7fc81f52439d574
xiaohui <1406744100@qq.com>
2018-11-18 13:34:40 +0800

    d

--- learningProcess/selfProject/ComTableW/comtablew.cpp
+++ learningProcess/selfProject/ComTableW/comtablew.cpp
@@ -4,3 +4,11 @@
 ComTableW::ComTableW()
 {
 }
+
+//为什么需要使用extern "C"呢？
+//C++之父在设计C++之时，考虑到当时已经存在了大量的C代码，为了支持原来的C代码和已经写好C库，需要在C++中尽可能的支持C，而extern "C"就是其中的一个策略
+
+extern  "C"  ComTableW* COMTABLEWSHARED_EXPORT HANDLE()
+{
+    return new ComTableW;
+}
